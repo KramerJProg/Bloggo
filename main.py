1a46d7f4e11956b128c10a6f7e078be86b830000
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-01-06 11:47:06 -0500

    Removed Secret (#37)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-from ignored.secret import the_secret
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
