8b7f5dc75b15cb83944a13552e15c76f9195df84
Kramer Jensen <kramerdjensen@gmail.com>
2024-01-06 16:10:03 -0500

    Page reloads where user refreshed it

---
+++ static/js/page-reloader.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+                var scrollpos = sessionStorage.getItem('scrollpos');
+                if (scrollpos) {
+                    window.scrollTo(0, scrollpos);
+                    sessionStorage.removeItem('scrollpos')
+                }
+            });
+
+            window.addEventListener("beforeunload", function(e)  {
+                sessionStorage.setItem('scrollpos', window.scrollY);
+            });
