d25014974b9a67a2026733542bafd7f8b8e7a0a8
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2024-01-06 16:10:38 -0500

    Page reloads where user refreshed it (#40)
    Co-authored-by: Kramer Jensen <kramerdjensen@gmail.com>

--- main.py
+++ main.py
@@ -193,6 +193,7 @@ def show_post(post_id):
         )
         db.session.add(new_comment)
         db.session.commit()
+
     return render_template('post.html', post=requested_post, form=comment_form)
 
 
